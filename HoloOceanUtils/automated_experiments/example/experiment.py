781e7117be5dcd91c75d8825531371fc21422f4d
Jungseok Hong <ssjason.hong@gmail.com>
2023-11-30 17:25:13 -0500

    exp4 with bearing updated

--- HoloOceanUtils/automated_experiments/example/experiment.py
+++ HoloOceanUtils/automated_experiments/example/experiment.py
@@ -3,7 +3,7 @@ from py_factor_graph.io.pyfg_text import read_from_pyfg_text
 
 
 if __name__ == '__main__':
-    run_experiment("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/experiment_2.json")
+    run_experiment("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/experiment_3.json")
     # pyfg = read_from_pyfg_text("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/test_0.pyfg")
     # pyfg.animate_odometry_3d(show_gt=True, draw_range_lines=True, num_timesteps_keep_ranges = 1)
     # pyfg = read_from_pyfg_text("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/test_1.pyfg")
